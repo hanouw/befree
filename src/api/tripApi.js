7cde41675729410de3be13a58d4cf9189ac13abb
hanouw <jehasite@gmail.com>
2024-06-10 12:24:39 +0900

    api

--- src/api/tripApi.js
+++ src/api/tripApi.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+// const serviceKey =
+//   "uO3hkVTS0Jua91aVTLwTYLDL9n1Tta108iwJEvwieZmxcwtzO32Fk9cyhgaKc5A22IM%2FREUAIyCVKvoTbvnfmg%3D%3D";
+const serviceKey =
+  "K2%2FtyPnwaOZfrMCvSUG10bEQaU8GaFxghNI2voZCpUhGx2UALE2Hn3aXUw4cc0xBYxt%2FWGf%2FoSPRSzd8XuhKvA%3D%3D";
+
 // 이미지 드래그 못하게 하는 style
 const noDrag = {
   userSelect: "none",
@@ -18,7 +23,7 @@ const noDrag = {
 export const disableData = async (contentId) => {
   // 컨텐츠ID가 “129619”인 무장애여행 정보 조회
   const response = await axios.get(
-    `http://apis.data.go.kr/B551011/KorWithService1/detailWithTour1?serviceKey=uO3hkVTS0Jua91aVTLwTYLDL9n1Tta108iwJEvwieZmxcwtzO32Fk9cyhgaKc5A22IM%2FREUAIyCVKvoTbvnfmg%3D%3D&contentId=${contentId}&MobileOS=ETC&MobileApp=Befree&_type=json`
+    `http://apis.data.go.kr/B551011/KorWithService1/detailWithTour1?serviceKey=${serviceKey}&contentId=${contentId}&MobileOS=ETC&MobileApp=Befree&_type=json`
   );
   return response.data;
 };
@@ -66,7 +71,7 @@ export const placeKeywordData = async (
     }
 
     let requestLink =
-      `http://apis.data.go.kr/B551011/KorWithService1/${keyUrl}?serviceKey=uO3hkVTS0Jua91aVTLwTYLDL9n1Tta108iwJEvwieZmxcwtzO32Fk9cyhgaKc5A22IM%2FREUAIyCVKvoTbvnfmg%3D%3D` +
+      `http://apis.data.go.kr/B551011/KorWithService1/${keyUrl}?serviceKey=${serviceKey}` +
       `&numOfRows=${numOfRows}` +
       "&MobileOS=ETC" +
       "&listYN=Y" +
@@ -129,17 +134,39 @@ export const sendPlaceKeywordDataApi = async (recentResult) => {
       null, //cat2
       6 // 한번에 요청하는 개수
     );
-    console.log(data);
-    placeDataList = data.response.body.items.item;
+
+    const item = data.response.body.items.item;
+
+    // 장소 저장
+    placeDataList = item;
+
+    // 장소 시설 탐색
+    for (let i = 0; i < item.length; i++) {
+      console.log("장소 시설 탐색중...");
+      let tempFacil = [];
+      const conId = item[i].contentid;
+
+      console.log(conId);
+
+      const disableReturnTotalData = await disableData(conId); // 여기에 데이터 저장
+      const disableReturnData =
+        disableReturnTotalData.response.body.items.item[0];
+
+      for (let key in disableReturnData) {
+        if (disableReturnData[key].trim() && key != "contentid") {
+          let value = disableReturnData[key].replace("_무장애 편의시설", "");
+          value = value.replace("_시각장애인 편의시설", "");
+          value = value.replace("_청각장애인 편의시설", "");
+          value = value.replace("_지체장애인 편의시설", "");
+          tempFacil.push(value);
+        }
+      }
+      facilitiesList.push(tempFacil);
+    }
   } else {
     let pageNum = 1;
     // while (pageNum < 6) {
     while (placeDataList.length < 6) {
-      console.log(
-        "==================================",
-        placeDataList.length,
-        pageNum
-      );
       const data = await placeKeywordData(
         pageNum, // pageNo
         recentResult.imgNece || "A", //arrange
@@ -152,7 +179,6 @@ export const sendPlaceKeywordDataApi = async (recentResult) => {
         1 // 한번에 요청하는 개수
       );
       pageNum++;
-      console.log(data);
       if (data.response.body.items == "") {
         break;
       }
@@ -168,8 +194,6 @@ export const sendPlaceKeywordDataApi = async (recentResult) => {
             disableReturnTotalData.response.body.items.item[0];
           const facilities = recentResult.facilityCodeArray;
 
-          console.log(disableReturnData);
-
           // 필터링
           const hasAvailableFacility = facilities.some((facility) =>
             disableReturnData[facility.id]?.trim()
@@ -179,41 +203,55 @@ export const sendPlaceKeywordDataApi = async (recentResult) => {
             // 시설을 저장
             let tempFacil = [];
             for (let key in disableReturnData) {
-              if (disableReturnData[key].trim()) {
-                tempFacil.push({ [key]: disableReturnData[key] });
+              if (disableReturnData[key].trim() && key != "contentid") {
+                let value = disableReturnData[key].replace(
+                  "_무장애 편의시설",
+                  ""
+                );
+                tempFacil.push(value);
               }
             }
             console.log("====True====");
             placeDataList.push(item);
             facilitiesList.push(tempFacil);
-          } else {
-            console.log("====False====");
           }
         } catch (error) {
           console.error("Error fetching disable data:", error);
         }
       } else {
-        placeDataList.push(data.response.body.items.item[0]);
-        // 시설을 저장
-        // let tempFacil = [];
-        // for (let key in disableReturnData) {
-        //   if (disableReturnData[key].trim()) {
-        //     tempFacil.push({ [key]: disableReturnData[key] });
-        //   }
-        // }
-        // facilitiesList.push(tempFacil);
+        const item = data.response.body.items.item;
+
+        // 장소 저장
+        placeDataList.push(item);
+
+        // 장소 시설 탐색
+        for (let i = 0; i < item.size; i++) {
+          console.log("장소 시설 탐색중...");
+          let tempFacil = [];
+          const conId = item[i].contentid;
+          const disableReturnTotalData = await disableData(conId); // 여기에 데이터 저장
+          const disableReturnData =
+            disableReturnTotalData.response.body.items.item[0];
+          for (let key in disableReturnData) {
+            if (disableReturnData[key].trim() && key != "contentId") {
+              let value = disableReturnData[key].replace(
+                "_무장애 편의시설",
+                ""
+              );
+              tempFacil.push(value);
+            }
+          }
+          facilitiesList.push(tempFacil);
+        }
       }
     }
   }
-
-  console.log(placeDataList);
 
   // 지도 만들기
   const newMap = []; // 새로운 배열 생성
   if (placeDataList.length != 0) {
     for (let i = 0; i < placeDataList.length; i++) {
-      // =================
-      const item = placeDataList[i]; // =================
+      const item = placeDataList[i];
       const mapObj = {
         mapx: item.mapx,
         mapy: item.mapy,
@@ -226,7 +264,7 @@ export const sendPlaceKeywordDataApi = async (recentResult) => {
   let newTripList = [];
 
   for (let i = 0; i < placeDataList.length; i++) {
-    const item = placeDataList[i]; // =================
+    const item = placeDataList[i];
     const tempPlace = {
       src:
         item.firstimage ||
@@ -237,6 +275,9 @@ export const sendPlaceKeywordDataApi = async (recentResult) => {
       address: item.addr1,
       style: noDrag,
       contentId: item.contentid,
+      facility: facilitiesList[i][0] ? facilitiesList[i] : null,
+      mapx: newMap[i].mapx,
+      mapy: newMap[i].mapy,
     };
     newTripList.push(tempPlace);
   }
