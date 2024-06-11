65be92a144b15f90fb7d38176d8f1d1c875a5520
Goldenhyo <jintop01@naver.com>
2024-06-11 16:09:44 +0900

    TripListDetail 날짜계산 로직, TripList 로그인 인증

--- src/pages/user/MyPage.js
+++ src/pages/user/MyPage.js
@@ -1,56 +1,75 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BasicLayout from "../../layouts/BasicLayout";
 import useCustomMove from "../../hooks/useCustomMove";
+import useCustomLogin from "../../hooks/useCustomLogin";
+import { useSelector } from "react-redux";
 
 const MyPage = () => {
-  const { moveToDelete, moveToModify } = useCustomMove();
-  const inputClassName =
-    "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 font-[Pretendard-Regular]";
+	const { moveToDelete, moveToModify, moveToLogin, moveToMain } = useCustomMove();
+	const inputClassName =
+		"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 font-[Pretendard-Regular]";
 
-  const buttonClassName =
-    "text-center w-full text-gray-900 inline-flex justify-center items-center bg-my-color-lightgreen hover:bg-my-color-superlightgreen border-black focus:ring-4 focus:outline-none focus:ring-my-color-lightgreen font-['Pretendard-Regular'] rounded-sm text-sm px-5 py-2.5";
+	const buttonClassName =
+		"text-center w-full text-gray-900 inline-flex justify-center items-center bg-my-color-lightgreen hover:bg-my-color-superlightgreen border-black focus:ring-4 focus:outline-none focus:ring-my-color-lightgreen font-['Pretendard-Regular'] rounded-sm text-sm px-5 py-2.5";
 
-  const emptyButtonClassName =
-    "text-center w-full text-gray-900 inline-flex justify-center items-center border border-my-color-lightgreen hover:bg-my-color-superlightgreen focus:ring-4 focus:outline-none focus:ring-my-color-lightgreen font-['Pretendard-Regular'] rounded-sm text-sm px-5 py-2.5 bg-white";
+	const emptyButtonClassName =
+		"text-center w-full text-gray-900 inline-flex justify-center items-center border border-my-color-lightgreen hover:bg-my-color-superlightgreen focus:ring-4 focus:outline-none focus:ring-my-color-lightgreen font-['Pretendard-Regular'] rounded-sm text-sm px-5 py-2.5 bg-white";
 
-  return (
-    <BasicLayout>
-      <div className="font-[Pretendard-Bold] text-3xl grid place-items-center mt-12 mb-12">내 정보</div>
-      <div className="w-full grid place-items-center gap-5">
-        <div className="w-full sm:w-1/2 md:w-1/3 px-4">
-          <span className="font-[Pretendard-Regular]">이메일 주소</span>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            className={inputClassName}
-            placeholder="Befree@befree.com"
+	const { isLogin } = useCustomLogin();
+  const loginState = useSelector((state) => state.loginSlice);
+  useEffect(() => {
+		console.log("isLogin:", isLogin);
+		if (!isLogin) {
+			const isConfirmed = window.confirm(
+				"로그인이 필요합니다. 로그인 페이지로 이동하시겠습니까?"
+			);
+			if (isConfirmed) {
+				moveToLogin();
+			} else {
+				moveToMain();
+			}
+		}
+	}, []);
+	return (
+		<BasicLayout>
+			<div className="font-[Pretendard-Bold] text-3xl grid place-items-center mt-12 mb-12">
+				내 정보
+			</div>
+			<div className="w-full grid place-items-center gap-5">
+				<div className="w-full sm:w-1/2 md:w-1/3 px-4">
+					<span className="font-[Pretendard-Regular]">이메일 주소</span>
+					<input
+						id="email"
+						name="email"
+						type="email"
+						className={inputClassName}
+						value={loginState.email}
             disabled
-          />
-        </div>
-        <div className="w-full sm:w-1/2 md:w-1/3 px-4 mb-4">
-          <span className="font-[Pretendard-Regular]">비밀번호</span>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            className={inputClassName}
-            placeholder="8자리 이상의 비밀번호"
-            value={"********"}
-            disabled
-          />
-        </div>
-        <div className="w-full sm:w-1/2 md:w-1/3 px-4 flex gap-10">
-          <button className={emptyButtonClassName} onClick={moveToDelete}>
-            회원 탈퇴
-          </button>
-          <button className={buttonClassName} onClick={moveToModify}>
-            정보수정
-          </button>
-        </div>
-      </div>
-    </BasicLayout>
-  );
+					/>
+				</div>
+				<div className="w-full sm:w-1/2 md:w-1/3 px-4 mb-4">
+					<span className="font-[Pretendard-Regular]">비밀번호</span>
+					<input
+						id="password"
+						name="password"
+						type="password"
+						className={inputClassName}
+						placeholder="8자리 이상의 비밀번호"
+						value={"********"}
+						disabled
+					/>
+				</div>
+				<div className="w-full sm:w-1/2 md:w-1/3 px-4 flex gap-10">
+					<button className={emptyButtonClassName} onClick={moveToDelete}>
+						회원 탈퇴
+					</button>
+					<button className={buttonClassName} onClick={moveToModify}>
+						정보수정
+					</button>
+				</div>
+			</div>
+		</BasicLayout>
+	);
 };
 
 export default MyPage;
