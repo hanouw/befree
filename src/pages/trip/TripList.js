6b3fc0993f11185b2cb6a039d46d1f5485438bc3
hanouw <jehasite@gmail.com>
2024-06-13 17:11:35 +0900

    tripList 수정 추가

--- src/pages/trip/TripList.js
+++ src/pages/trip/TripList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import TripAddModalComponent from "../../components/tripList/TripAddModalComponent";
+import TripModifyModalComponent from "../../components/tripList/TripModifyModalComponent";
 import useCustomMove from "../../hooks/useCustomMove";
 import PagenationComponent from "../../components/tripList/PagenationComponent";
 import { getTripList } from "../../api/befreeApi";
@@ -11,6 +12,9 @@ const TripList = () => {
   const { moveToTripListDetail, moveToMain, moveToLogin } = useCustomMove();
   // 모달 띄우기 여부
   const [showModal, setShowModal] = useState(false);
+  const [showModifyModal, setShowModifyModal] = useState(false);
+  const [modifyModalData, setModifyModalData] = useState(null);
+
   const [tripList, setTripList] = useState([]);
   const [page, setPage] = useState(1);
   const [totalPage, setTotalPage] = useState(0);
@@ -35,20 +39,62 @@ const TripList = () => {
     fetchTripList(page);
   };
 
-	const numButtonClicked = (buttonNumber) => {
-		setPage(buttonNumber);
-		console.log("눌림", buttonNumber);
-	};
+  // 수정 모달 상태 변화 함수
+  const modifyModalStateChange = ({ tid, title, region, date }) => {
+    if (showModifyModal) {
+      // true 모달이 꺼지는 상황
+      setShowModifyModal(false);
+      setModifyModalData(null);
+      console.log("모달 꺼진다");
+    } else {
+      // false 모달이 켜지는 상황
+      setShowModifyModal(true);
+      if (
+        tid != undefined &&
+        title != undefined &&
+        region != undefined &&
+        date != undefined
+      ) {
+        const formatDate = (dateString) => {
+          const [year, month, day] = dateString.split(".");
 
-	const loginInfo = useSelector((state) => state.loginSlice);
+          const formattedMonth = month.length === 1 ? "0" + month : month;
+          const formattedDay = day.length === 1 ? "0" + day : day;
 
-	const fetchTripList = (page) => {
-		console.log("fetchTripList 호출됨 email", loginInfo.email);
-		getTripList(loginInfo.email, page).then((data) => {
-			console.log("안쪽");
-			let dataResult = data.RESULT.paginatedTrips;
-			setTotalPage(data.RESULT.totalPage);
-			console.log(dataResult);
+          return `${year}-${formattedMonth}-${formattedDay}`;
+        };
+
+        const begin = formatDate(date.split(" ~ ")[0]);
+        const end = formatDate(date.split(" ~ ")[1]);
+
+        console.log(begin);
+        console.log(end);
+        setModifyModalData({
+          tid: tid,
+          title: title,
+          region: region,
+          begin: begin,
+          end: end,
+        });
+      }
+    }
+    fetchTripList(page);
+  };
+
+  const numButtonClicked = (buttonNumber) => {
+    setPage(buttonNumber);
+    console.log("눌림", buttonNumber);
+  };
+
+  const loginInfo = useSelector((state) => state.loginSlice);
+
+  const fetchTripList = (page) => {
+    console.log("fetchTripList 호출됨 email", loginInfo.email);
+    getTripList(loginInfo.email, page).then((data) => {
+      console.log("안쪽");
+      let dataResult = data.RESULT.paginatedTrips;
+      setTotalPage(data.RESULT.totalPage);
+      console.log(dataResult);
 
       const tripListTemp = dataResult.map((trip) => {
         const tbegin = new Date(trip.tbegin);
@@ -100,7 +146,21 @@ const TripList = () => {
     <>
       {showModal ? (
         <TripAddModalComponent callbackFn={modalStateChange} />
-      ) : null}
+      ) : (
+        <></>
+      )}
+      {showModifyModal ? (
+        <TripModifyModalComponent
+          tid={modifyModalData.tid}
+          title={modifyModalData.title}
+          region={modifyModalData.region}
+          begin={modifyModalData.begin}
+          end={modifyModalData.end}
+          callbackFn={modifyModalStateChange}
+        />
+      ) : (
+        <></>
+      )}
       {/* 상단 이미지 */}
       <div
         className="w-full mb-8 sm:mb-24 flex justify-center items-center"
@@ -130,25 +190,80 @@ const TripList = () => {
       </div>
       <div className="grid place-items-center lg:mt-10 mt-3">
         {tripList.map((item) => (
-          <div
-            className="w-2/3 lg:w-mywidth1200"
-            key={item.src}
-            onClick={() =>
-              moveToTripListDetail(item.alt, item.title, item.date, item.region)
-            }
-          >
+          <div className="w-2/3 lg:w-mywidth1200" key={item.src}>
             <img
               src={item.src}
               alt={item.alt}
               style={item.style}
-              className="rounded-md h-14 sm:h-full"
+              className="rounded-md h-14 sm:h-full hover:cursor-pointer"
+              onClick={() =>
+                moveToTripListDetail(
+                  item.alt,
+                  item.title,
+                  item.date,
+                  item.region
+                )
+              }
             ></img>
-            <div className="sm:mt-2 mb-4">
-              <span className="font-[Pretendard-Light] text-sm sm:text-lg">
-                {item.date}
-              </span>
-              <div className="font-[Pretendard-Light] text-sm sm:text-lg text-gray-600">
-                {item.title}
+            <div className="flex justify-between">
+              <div
+                className="sm:mt-2 mb-4 hover:cursor-pointer"
+                onClick={() =>
+                  moveToTripListDetail(
+                    item.alt,
+                    item.title,
+                    item.date,
+                    item.region
+                  )
+                }
+              >
+                <span className="font-[Pretendard-Light] text-sm sm:text-lg">
+                  {item.date}
+                </span>
+                <div className="font-[Pretendard-Light] text-sm sm:text-lg text-gray-600">
+                  {item.title}
+                </div>
+              </div>
+              <div className="flex items-center sm:mt-2 mb-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-7 mr-4 cursor-pointer hover:mb-1"
+                  onClick={() =>
+                    alert("여행 공유 탭에 추가 됩니다. 기능 구현중...")
+                  }
+                >
+                  <path
+                    strokelnecap="round"
+                    strokelnejoin="round"
+                    d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z"
+                  />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-8 mr-4 cursor-pointer hover:mb-1"
+                  onClick={() =>
+                    modifyModalStateChange({
+                      tid: item.alt,
+                      title: item.title,
+                      region: item.region,
+                      date: item.date,
+                    })
+                  }
+                >
+                  <path
+                    strokelnecap="round"
+                    strokelnejoin="round"
+                    d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75"
+                  />
+                </svg>
               </div>
             </div>
           </div>
--- src/pages/trip/TripListDetailModifyDisignRef.js
+++ src/pages/trip/TripListDetailModifyDisignRef.js
@@ -70,13 +70,13 @@ const DraggableItem = ({ item, index, moveCard }) => {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="size-6"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLnecap="round"
+              strokeLnejoin="round"
               d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
             />
           </svg>
@@ -94,13 +94,13 @@ const DraggableItem = ({ item, index, moveCard }) => {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="size-6"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLnecap="round"
+              strokeLnejoin="round"
               d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
             />
           </svg>
@@ -173,13 +173,13 @@ const TripListDetail = () => {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
               className="size-6"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLnecap="round"
+                strokeLnejoin="round"
                 d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
               />
             </svg>
@@ -190,13 +190,13 @@ const TripListDetail = () => {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
               className="size-6"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLnecap="round"
+                strokeLnejoin="round"
                 d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
               />
             </svg>
