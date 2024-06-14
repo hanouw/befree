fefa805193d0acfd860dfd47e6df60eddfcf5174
Goldenhyo <jintop01@naver.com>
2024-06-14 12:10:01 +0900

    여행목록 공유하기 완성

--- src/pages/user/MyPage.js
+++ src/pages/user/MyPage.js
@@ -5,7 +5,7 @@ import useCustomLogin from "../../hooks/useCustomLogin";
 import { useSelector } from "react-redux";
 
 const MyPage = () => {
-	const { moveToDelete, moveToModify, moveToLogin, moveToMain } = useCustomMove();
+	const { moveToDelete, moveToLogin, moveToMain } = useCustomMove();
 	const inputClassName =
 		"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 font-[Pretendard-Regular]";
 
